7ef5d9781cbe3bd9d1f836d2a4f7b3670c4bf144
Chen <a8527369@gmail.com>
2022-11-11 12:21:23 +0800

    edit update

--- Update/Program.cs
+++ Update/Program.cs
@@ -11,14 +11,18 @@ namespace Update
 	{
 		static void Main(string[] args)
 		{
-			string sql = @"UPDATE Users_table SET Name=@Name, Password=@Password
-											WHERE Id = @Id";
+			string sql = @"UPDATE Users_table SET Name=@Name, Account=@Account, Password=@Password,
+												  DateOfBirthd=@DateOfBirthd, Height=@Height
+												  WHERE Id = @Id";
 			var dbHelpers = new SqlDbHelper("default");
 
 			try
 			{
-				var parameters = new SqlParameterBuilder().AddNVarchar("Name", 50, "Chu")
-															  .AddNVarchar("Password", 50, "11111")
+				var parameters = new SqlParameterBuilder().AddNVarchar("Name", 50, "Judy")
+															  .AddNVarchar("Account", 50, "3344")
+															  .AddNVarchar("Password", 50, "dsaddadasd")
+															  .AddDateTime("DateOfBirthd", new DateTime(2000, 01, 01))
+															  .AddInt("Height", 190)
 															  .AddInt("Id", 2)
 															  .Build();
 				dbHelpers.ExecuteNonQuery(sql, parameters);
